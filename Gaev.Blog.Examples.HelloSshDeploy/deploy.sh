4daf7941e524f32fa498e211e72c5c73cdea67d1
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2019-03-21 19:20:01 +0100

    HelloSshDeploy

--- Gaev.Blog.Examples.HelloSshDeploy/deploy.sh
+++ Gaev.Blog.Examples.HelloSshDeploy/deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 ssh root@192.168.2.4 'bash -s' <<'ENDSSH'
+printf "Stopping service...\n"
 systemctl stop HelloSshDeploy
+printf "Service is "
 systemctl is-active HelloSshDeploy
 mkdir -p /apps/HelloSshDeploy
 ENDSSH
 
+printf "Uploading new version of service...\n"
 rsync -v -a ./bin/Release/netcoreapp2.2/ubuntu.16.04-x64/publish/ root@192.168.2.4:/apps/HelloSshDeploy/
 
 ssh root@192.168.2.4 'bash -s' <<'ENDSSH'
 chmod 777 /apps/HelloSshDeploy/Gaev.Blog.Examples.HelloSshDeploy
 if [[ ! -e /etc/systemd/system/HelloSshDeploy.service ]]; then
-    # Create SystemD service file
+    printf "Installing service...\n"
     cat > /etc/systemd/system/HelloSshDeploy.service <<'EOF'
     [Unit]
     Description=HelloSshDeploy
@@ -28,6 +31,8 @@ EOF
     systemctl daemon-reload
     systemctl enable HelloSshDeploy
 fi
+printf "Starting service...\n"
 systemctl start HelloSshDeploy
+printf "Service is "
 systemctl is-active HelloSshDeploy
 ENDSSH
