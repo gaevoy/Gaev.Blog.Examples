3d3bd9fe69ef23c3ca275fb2770889544cdc2b41
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2019-03-21 21:47:53 +0100

    HelloSshDeploy

--- Gaev.Blog.Examples.HelloSshDeploy/Program.cs
+++ Gaev.Blog.Examples.HelloSshDeploy/Program.cs
@@ -11,11 +11,7 @@ namespace Gaev.Blog.Examples
         static async Task Main(string[] args)
         {
             var cancellation = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) =>
-            {
-                e.Cancel = true;
-                cancellation.Cancel();
-            };
+            Console.CancelKeyPress += (_, e) => { e.Cancel = true; cancellation.Cancel(); };
             using (var logger = new LoggerConfiguration()
                 .WriteTo.Console()
                 .WriteTo.File("HelloSshDeploy.log")
@@ -30,21 +26,8 @@ namespace Gaev.Blog.Examples
             {
                 await Task.Delay(Timeout.Infinite, cancellation);
             }
-            catch (TaskCanceledException)
-            {
-            }
+            catch (TaskCanceledException) { }
             logger.Information("Goodbye World!");
         }
     }
-
-    /*
-     
-     prerequisites: password-less ssh access; using root user; rsync (Linux for Windows is perfect); ubuntu 16+; 
-https://kimsereyblog.blogspot.com/2018/03/install-dotnet-on-ubuntu-with-linux.html
-https://dotnet.microsoft.com/download/linux-package-manager/ubuntu18-04/sdk-current
-https://florianbrinkmann.com/en/3436/ssh-key-and-the-windows-subsystem-for-linux/
-https://pmcgrath.net/running-a-simple-dotnet-core-linux-daemon
-https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-apache?view=aspnetcore-2.1&tabs=aspnetcore2x#create-the-service-file
-     * 
-     */
 }
