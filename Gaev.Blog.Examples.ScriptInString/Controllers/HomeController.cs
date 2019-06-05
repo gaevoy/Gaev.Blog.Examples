33a0d8c596afe5c3498afa6d626624265f908b2c
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2019-06-05 18:59:00 +0200

    Script tag in string

---
+++ Gaev.Blog.Examples.ScriptInString/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Gaev.Blog.Examples.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() => View("Problem1");
+        public ActionResult Problem1() => View();
+        public ActionResult Problem2() => View();
+        public ActionResult Fix1() => View();
+        public ActionResult Fix2() => View();
+    }
+}
---
+++ Gaev.Blog.Examples.ScriptInString/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Gaev.Blog.Examples
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{action}/{id}",
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+            );
+        }
+    }
+}
