2fc34479ea5baf56696d86f42cb2039fd049197d
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2019-06-26 09:49:54 +0200

    How to test serialization/deserialization

---
+++ Gaev.Blog.Examples.SerializationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Gaev.Blog.Examples.SerializationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gaev.Blog.Examples.SerializationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("CED62EC3-C199-4124-9A68-83D5349F5FDA")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Gaev.Blog.Examples.SerializationTests/SerializationTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
+using Gaev.Blog.Examples.Messages;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Gaev.Blog.Examples
+{
+    public class SerializationTests
+    {
+        [Test]
+        public void It_should_serialize_then_deserialize_UserRegistered()
+        {
+            // Given
+            var random = TestContext.CurrentContext.Random;
+            var givenMessage = new UserRegistered(
+                id: random.NextGuid(),
+                email: random.GetString(),
+                name: random.GetString());
+
+            // When
+            var json = JsonConvert.SerializeObject(givenMessage);
+            var deserializedMessage = JsonConvert.DeserializeObject<UserRegistered>(json);
+
+            // Then
+            Assert.That(deserializedMessage.Id, Is.EqualTo(givenMessage.Id));
+            Assert.That(deserializedMessage.Email, Is.EqualTo(givenMessage.Email));
+            Assert.That(deserializedMessage.Name, Is.EqualTo(givenMessage.Name));
+        }
+
+        [Test]
+        public void It_should_serialize_then_deserialize_UserRegistered_entirely()
+        {
+            // Given
+            var givenMessage = new Fixture().Create<UserRegistered>();
+
+            // When
+            var json = JsonConvert.SerializeObject(givenMessage);
+            var deserializedMessage = JsonConvert.DeserializeObject<UserRegistered>(json);
+
+            // Then
+            deserializedMessage.Should().BeEquivalentTo(givenMessage);
+        }
+
+        [TestCaseSource(nameof(AllMessageTypes))]
+        public void It_should_serialize_then_deserialize(Type messageType)
+        {
+            // Given
+            var givenMessage = new SpecimenContext(new Fixture()).Resolve(messageType); // https://github.com/AutoFixture/AutoFixture/issues/97#issuecomment-17064685
+
+            // When
+            var json = JsonConvert.SerializeObject(givenMessage);
+            var deserializedMessage = JsonConvert.DeserializeObject(json, messageType);
+
+            // Then
+            deserializedMessage.Should().BeEquivalentTo(givenMessage);
+        }
+
+        private static IEnumerable<Type> AllMessageTypes =>
+            AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && t.Namespace == "Gaev.Blog.Examples.Messages");
+    }
+}
+
+namespace Gaev.Blog.Examples.Messages
+{
+    public class UserRegistered
+    {
+        public UserRegistered(Guid id, string email, string name)
+        {
+            Id = id;
+            Email = email;
+            Name = name;
+        }
+
+        public Guid Id { get; }
+        public string Email { get; }
+        public string Name { get; }
+    }
+
+    public class UserRegistered_V2
+    {
+        public UserRegistered_V2(Guid id, string email, string name)
+        {
+            Id = id;
+            Login = email;
+            Name = name;
+        }
+
+        public Guid Id { get; }
+        public string Login { get; }
+        public string Name { get; }
+    }
+}
