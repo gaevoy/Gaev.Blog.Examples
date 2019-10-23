fa98abf73486098be3f0e8c7e819b04e1d52ab95
Vladimir Gaevoy <vova.gaevoy@gmail.com>
2019-10-23 23:00:02 +0200

    Task scheduler via Azure Service Bus

---
+++ Gaev.Blog.AzureServiceBusTaskScheduler/AzureTaskScheduler.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace Gaev.Blog.AzureServiceBusTaskScheduler
+{
+    public class AzureTaskScheduler
+    {
+        private readonly string _connectionString;
+
+        public AzureTaskScheduler(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task Run(
+            string queueName,
+            Func<Message, Task<Message>> task,
+            CancellationToken cancellation
+        )
+        {
+            var client = new QueueClient(_connectionString, queueName);
+            var created = await EnsureQueueCreatedAsync(client.QueueName);
+            if (created)
+                await client.SendAsync(await task(null));
+            client.RegisterMessageHandler(async (message, _) =>
+            {
+                var next = await task(message);
+                if (next != null)
+                    await client.SendAsync(next);
+            }, new MessageHandlerOptions(exceptionReceivedHandler: _ => Task.CompletedTask));
+
+            try
+            {
+                await Task.Delay(Timeout.Infinite, cancellation);
+            }
+            catch (TaskCanceledException)
+            {
+            }
+
+            await client.CloseAsync();
+        }
+
+        private async Task<bool> EnsureQueueCreatedAsync(string queueName)
+        {
+            var client = new ManagementClient(_connectionString);
+            if (!await client.QueueExistsAsync(queueName))
+            {
+                await client.CreateQueueAsync(queueName);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- Gaev.Blog.AzureServiceBusTaskScheduler/Program.cs
+++ Gaev.Blog.AzureServiceBusTaskScheduler/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Cronos;
+using Microsoft.Azure.ServiceBus;
 
 namespace Gaev.Blog.AzureServiceBusTaskScheduler
 {
@@ -16,9 +18,26 @@ namespace Gaev.Blog.AzureServiceBusTaskScheduler
                 e.Cancel = true;
                 cancellation.Cancel();
             };
-            var handler = new HealthyDeveloperHandler();
-            var queue = new SchedulerQueue(ConnectionString);
-            await queue.Subscribe(handler, cancellation.Token);
+            var scheduler = new AzureTaskScheduler(ConnectionString);
+            await scheduler.Run(queueName: "TakeABreak", task: Process, cancellation.Token);
+        }
+
+        static async Task<Message> Process(Message message)
+        {
+            if (message != null)
+            {
+                Console.WriteLine($"Take a break! It is {message.ScheduledEnqueueTimeUtc.ToLocalTime():t}.");
+                await Task.Delay(100);
+            }
+
+            return new Message
+            {
+                ScheduledEnqueueTimeUtc = CronExpression
+                    //.Parse("*/30 8-18 * * MON-FRI")
+                    .Parse("*/1 * * * MON-FRI")
+                    .GetNextOccurrence(DateTime.UtcNow, TimeZoneInfo.Local)
+                    .Value
+            };
         }
     }
 }
