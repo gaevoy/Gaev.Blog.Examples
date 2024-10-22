4de95a2404571622a20036f5b1a90a0abdc655d5
Volodymyr Gaevoy <vova.gaevoy@gmail.com>
2024-10-22 09:45:45 +0200

    C# 12 in .NET Framework

---
+++ Gaev.Blog.CSharp12AndNetFramework/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Gaev.Blog.CSharp12AndNetFramework;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        ReadOnlySpan<int> numbers = [1, 2, 3, 4, 5, 6];
+        // Index
+        int secondToLast = numbers[^2];
+        // Range
+        var firstFour = numbers[..4];
+        // List pattern
+        if (numbers is [_, 2, var third, .. var rest])
+        {
+        }
+    }
+
+    // Records
+    public record Person(string FirstName, string LastName);
+
+    // Required member, init
+    public class Config
+    {
+        public required string ConnectionString { get; init; }
+    }
+}
